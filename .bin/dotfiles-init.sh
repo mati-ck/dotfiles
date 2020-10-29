50366dd247701fe5105b817e8af2565547f35a0d
Mateusz Pajewski <pajewski.m@gmail.com>
2020-10-29 18:55:20 +0100

    Dotfiles bare repository scripts

---
+++ .bin/dotfiles-init.sh
@@ -0,0 +1,4 @@
+git init --bare $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+config config --local status.showUntrackedFiles no
+echo "alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
